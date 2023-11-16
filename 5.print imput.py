efd14750bb5113fd7cd860be0d0d42fc71e85891
Orlando Junior <orlandojsjunior@hotmail.com>
2023-11-16 00:58:51 -0300

    Operadores de Atribuição

--- 5.print imput.py
+++
@@ -1,9 +0,0 @@
-nome = input ("Qual o seu nome: ")
-idade = input ("Qual sua idade?: ")
-
-print(nome, idade)
-print(nome, idade, end=" ...\n")
-print(nome, idade, sep="#", end="...\n")
-print(nome, idade, sep="#")
-
-
